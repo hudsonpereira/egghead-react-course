8d0145b66c20129d8b8faff63479dd87ecb0cc30
Hudson Pereira <hudson.byte@gmail.com>
2019-01-31 15:09:40 -0200

    more

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            txt: 'this is the state txt',
+            cat: 0
+        }
+    }
+
+    update(e) {
+        this.setState({
+            txt: e.target.value
+        })
+    }
+
     render() {
-        return React.createElement('h1', {}, 'Hello world');
+        return <Button>I <Heart /> React</Button>
     }
 }
+
+const Button = (props) => <button>{props.children}</button>
+class Heart extends React.Component {
+    render() {
+        return <span>&hearts;</span>
+    }
+}
+
+App.propTypes = {
+    txt: PropTypes.string.isRequired,
+    cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: 'this is the default'
+}
+
 export default App;
